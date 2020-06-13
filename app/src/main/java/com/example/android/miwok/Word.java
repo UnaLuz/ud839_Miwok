1f04390fd372f2119dff0aeb687f29ae1af0f03e
UnaLuz <luz.cucarella97@gmail.com>
2020-06-13 17:25:51 -0300

    Stylized the activities Added images to the list items, using a placeholder for the moment And changed the Word class and the Word adapter to be able to hold the images and display them when necessary

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,29 +4,47 @@ package com.example.android.miwok;
  * Stores a miwok word and it's default translation
  */
 public class Word {
-    protected String defaultWord = "";
-    protected String miwokWord = "";
+    private int NO_IMAGE = -1;
+    private String mDefaultWord;
+    private String mMiwokWord;
+    private int mImageResourceId = NO_IMAGE;
 
     /**
      * Receives the default translation and the miwok word
      */
     public Word(String defWord, String miwWord) {
-        defaultWord = defWord;
-        miwokWord = miwWord;
+        mDefaultWord = defWord;
+        mMiwokWord = miwWord;
+    }
+
+    /**
+     * Receives the default translation and the miwok word
+     */
+    public Word(String defWord, String miwWord, int resId) {
+        mDefaultWord = defWord;
+        mMiwokWord = miwWord;
+        mImageResourceId = resId;
     }
 
     /**
      * @return the default translation of the miwok word
      */
     public String getDefaultWord() {
-        return defaultWord;
+        return mDefaultWord;
     }
 
     /**
      * @return the miwok word
      */
     public String getMiwokWord() {
-        return miwokWord;
+        return mMiwokWord;
+    }
+
+    /**
+     * @return the image resource of the miwok word
+     */
+    public int getImageResource() {
+        return mImageResourceId;
     }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -23,19 +24,27 @@ public class WordAdapter extends ArrayAdapter<Word> {
                     R.layout.list_item, parent, false);
         }
 
-        // Get the {@link AndroidFlavor} object located at this position in the list
+        // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID version_number
-        TextView miwokTextView = listItemView.findViewById(R.id.miwok_number_text_view);
-        // Get the version number from the current AndroidFlavor object and
-        // set this text on the number TextView
+        // Find the TextView in the list_item.xml layout with the ID image_view
+        ImageView imageView = listItemView.findViewById(R.id.image_view);
+        if(currentWord.getImageResource() != -1){
+            // Get the corresponding image resource and set it in the image view of the list
+            imageView.setImageResource(currentWord.getImageResource());
+        }
+        else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_word_text_view
+        TextView miwokTextView = listItemView.findViewById(R.id.miwok_word_text_view);
+        // Get the corresponding miwok word and set it in the text view of the list
         miwokTextView.setText(currentWord.getMiwokWord());
 
-        // Find the TextView in the list_item.xml layout with the ID version_name
-        TextView defaultTextView = listItemView.findViewById(R.id.default_number_text_view);
-        // Get the version name from the current AndroidFlavor object and
-        // set this text on the name TextView
+        // Find the TextView in the list_item.xml layout with the ID default_word_text_view
+        TextView defaultTextView = listItemView.findViewById(R.id.default_word_text_view);
+        // Get the corresponding default translation and set it in the text view of the list
         defaultTextView.setText(currentWord.getDefaultWord());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
