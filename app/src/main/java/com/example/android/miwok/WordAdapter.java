2528dd96dbcfcc76adb60675bcb4da6ff2510cd3
UnaLuz <luz.cucarella97@gmail.com>
2020-06-16 19:47:26 -0300

    Added a white arrow to the list_item.xml so it's easier for the user to know that they can play the audios Also added an animation for when the user clicks on a category in the main activity or on an item from the lists Made some improvements to the structure of the layouts and deleted the main activity for the landscape mode since the new main layout is good for both portrait and landscape mode

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -30,31 +30,31 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
+        if (currentWord != null) {
+            // Find the TextView in the list_item.xml layout with the ID image_view
+            ImageView imageView = listItemView.findViewById(R.id.image_view);
+            if (currentWord.getImageResource() != -1) {
+                // Get the corresponding image resource and set it in the image view of the list
+                imageView.setImageResource(currentWord.getImageResource());
+            } else {
+                imageView.setVisibility(View.GONE);
+            }
 
-        // Find the TextView in the list_item.xml layout with the ID image_view
-        ImageView imageView = listItemView.findViewById(R.id.image_view);
-        if (currentWord.getImageResource() != -1) {
-            // Get the corresponding image resource and set it in the image view of the list
-            imageView.setImageResource(currentWord.getImageResource());
-        } else {
-            imageView.setVisibility(View.GONE);
+            // Find the TextView in the list_item.xml layout with the ID miwok_word_text_view
+            TextView miwokTextView = listItemView.findViewById(R.id.miwok_word_text_view);
+            // Get the corresponding miwok word and set it in the text view of the list
+            miwokTextView.setText(currentWord.getMiwokWord());
+
+            // Find the TextView in the list_item.xml layout with the ID default_word_text_view
+            TextView defaultTextView = listItemView.findViewById(R.id.default_word_text_view);
+            // Get the corresponding default translation and set it in the text view of the list
+            defaultTextView.setText(currentWord.getDefaultWord());
+
+            // Get the view that contains all views in the list item
+            View textContainer = listItemView.findViewById(R.id.container_view);
+            // Set it's background color to mBgColorRes
+            textContainer.setBackgroundResource(mBgColorResId);
         }
-
-        // Find the TextView in the list_item.xml layout with the ID miwok_word_text_view
-        TextView miwokTextView = listItemView.findViewById(R.id.miwok_word_text_view);
-        // Get the corresponding miwok word and set it in the text view of the list
-        miwokTextView.setText(currentWord.getMiwokWord());
-
-        // Find the TextView in the list_item.xml layout with the ID default_word_text_view
-        TextView defaultTextView = listItemView.findViewById(R.id.default_word_text_view);
-        // Get the corresponding default translation and set it in the text view of the list
-        defaultTextView.setText(currentWord.getDefaultWord());
-
-        // Get the view that contains both text views in the list item
-        View textContainer = listItemView.findViewById(R.id.text_container);
-        // Set it's background color to mBgColorRes
-        textContainer.setBackgroundResource(mBgColorResId);
-
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
